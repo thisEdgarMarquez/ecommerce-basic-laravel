ed557c92d9cc1654b2154676d17a813c5306691c
Edgar Marquez <blaackhat@gmail.com>
2018-04-13 14:31:07 -0400

    Se agrego la función de actualizar,se hicieron un performance y se agrego paginación

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Categoria;
 
 class CategoriaController extends Controller
 {
     public function index(){
-        $categorias = Categoria::all();
+        $categorias = Categoria::paginate(10);
         return view('admin/categorias/index',['categorias' => $categorias]);
     }
     public function agregar(){
@@ -18,27 +19,33 @@ class CategoriaController extends Controller
         $this->validate($request,[
             'nombre' => 'required|unique:categorias|max:255',
             'descripcion' => 'string|required',
-            'estado' => 'required|boolean'
+            'status' => 'required|boolean'
         ]);
         $categoria = Categoria::create([
             'nombre' => $request->get('nombre'),
             'descripcion' => $request->get('descripcion'),
             'status' => $request->get('estado')
         ]);
-        $msj = $categoria ? 'La categoria fue creada con exito.' : 'Lo sentimos, ocurrio un error en el proceso de creación de la categoria.';
+        $msj = $categoria ? 'La categoria fue creada con exito.' : 'Lo sentimos, ocurrió un error en el proceso de creación de la categoria.';
         return redirect()->route('agregarCategoria')->with('msj',$msj);
     }
     public function eliminar(Request $request){
+        Categoria::findOrFail($request->get('id'));
         $deleted = Categoria::destroy($request->get('id'));
-        $msj = $deleted ? 'La categoria fue eliminada con exito.' : 'Lo sentimos, ocurrio un error en el proceso de eliminación de la categoria.';
+        $msj = $deleted ? 'La categoria fue eliminada con exito.' : 'Lo sentimos, ocurrió un error en el proceso de eliminación de la categoria.';
         return response()->json(['error'=>false,'msj' => $msj]);
     }
     public function actualizar(Request $request){
+        $categoria = Categoria::findOrFail($request->get('id'));
         $this->validate($request,[
-            'nombre' => 'required|unique:categorias|max:255',
+            'nombre' => ['required','max:255',Rule::unique('categorias')->ignore($request->get('id'))],
             'descripcion' => 'string|required',
-            'estado' => 'required|boolean'
+            'status' => 'required|boolean'
         ]);
+        $categoria->fill($request->all());
+        $actualizacion = $categoria->save();
+        $msj = $actualizacion ? 'La categoria fue actualizada con exito.' : 'Lo sentimos, ocurrió un error en el proceso de actualización de la categoria.';
+        return redirect()->back()->with('msj',$msj);
     }
     public function editar(Request $request){
         return view('admin/categorias/editar')->with('categoria',Categoria::findOrFail($request->segment(4)));
